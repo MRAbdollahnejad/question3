327c1e7a2c1bb5004f0b6c26d3c717c4ceed78c6
muhammadreza abdollahnejad <reza123.reza987@gmail.com>
2023-03-15 18:19:57 +0330

    change in Master class and create Lesson class

---
+++ Lesson.java
@@ -0,0 +1,8 @@
+package maktab_question_three;
+
+public class Lesson {
+    private String name;
+    private int id;
+    private Master master;
+    
+}
--- Master.java
+++ Master.java
@@ -3,10 +3,14 @@ package maktab_question_three;
 public class Master {
     private String name;
     private int id;
+    private Lesson[] lessons;
+
 
     public Master(String name, int id) {
         this.name = name;
         this.id = id;
+
     }
-    
+
+
 }
