d895753f3084ea4cacbe733cc5ee98bb485f2e71
muhammadreza abdollahnejad <reza123.reza987@gmail.com>
2023-03-15 18:03:43 +0330

    create Master class

---
+++ Master.java
@@ -0,0 +1,12 @@
+package maktab_question_three;
+
+public class Master {
+    private String name;
+    private int id;
+
+    public Master(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+    
+}
